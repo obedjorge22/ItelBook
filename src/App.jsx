f088bac2a1ad0110ea3bf228ff8b2ec383a009a4
Obed jorge <Obedjorge22@gmail.com>
2025-11-25 02:55:02 +0100

    terceiro commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world{" "}
+      </h1>
+      <div className='m-7'>
+        <p className="text-2xl text-green-600">Obed Jorge</p>
+        <p className="text-2xl text-blue-600">Efraim Raul</p>
+      </div>
+    </>
+  );
+}
+
+export default App
